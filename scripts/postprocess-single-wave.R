913202ddbbb165e6183bd2ec03af6963bc553f8d
Shozen Dan <xiaoran.duan@gmail.com>
2023-02-28 10:39:00 +0900

    [debug] postprocessing

--- scripts/postprocess-single-wave.R
+++ scripts/postprocess-single-wave.R
@@ -75,30 +75,30 @@ source(file.path(cli_args$repo_path, "R/posterior_contact_intensity.R"))
 cat(" Assess convergence and mixing ...\n")
 
 # Make convergence diagnostic tables
-fit_summary <- convergence_diagnostic_stats(fit, outdir = export_path)
+# fit_summary <- convergence_diagnostic_stats(fit, outdir = export_path)
 
 # Make trace plots
 cat("\n Making trace plots")
 
-pars <- c('nu', 'gp_alpha', 'gp_rho_1', 'gp_rho_2')
-pars_po <- fit$draws(pars)
-p <- bayesplot::mcmc_trace(pars_po,
-                           facet_args = list(nrow = 10, ncol = 1))
-ggsave(file = file.path(export_fig_path, 'mcmc_trace_parameters.png'),
-       plot = p,
-       h = 20,
-       w = 20,
-       limitsize = F)
+# pars <- c('nu', 'gp_alpha', 'gp_rho_1', 'gp_rho_2')
+# pars_po <- fit$draws(pars)
+# p <- bayesplot::mcmc_trace(pars_po,
+#                            facet_args = list(nrow = 10, ncol = 1))
+# ggsave(file = file.path(export_fig_path, 'mcmc_trace_parameters.png'),
+#        plot = p,
+#        h = 20,
+#        w = 20,
+#        limitsize = F)
 
 # Make pairs plots
 cat(" Making pairs plots\n")
-p <- bayesplot::mcmc_pairs(pars_po,
-                           off_diag_args = list(size = 0.3, alpha = 0.3))
-ggsave(file = file.path(export_fig_path, 'mcmc_pairs_parameters.png'),
-       plot = p,
-       h = 20,
-       w = 20,
-       limitsize = F)
+# p <- bayesplot::mcmc_pairs(pars_po,
+#                            off_diag_args = list(size = 0.3, alpha = 0.3))
+# ggsave(file = file.path(export_fig_path, 'mcmc_pairs_parameters.png'),
+#        plot = p,
+#        h = 20,
+#        w = 20,
+#        limitsize = F)
 
 ##### ---------- Posterior predictive checks ---------- #####
 cat(" Extracting posterior ...\n")
